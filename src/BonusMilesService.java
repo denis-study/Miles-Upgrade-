30cecce115cdcf323028c83159b0d9332daba660
Denis T <dengertd@gmail.com>
2021-02-02 11:08:48 +0300

    ypgrade commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
     public  int calculate(int cost) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
         int count = 20;
-        int miles = (price / count);
+        int miles = (cost / count);
         return miles;
        }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
+        int cost = 10_000;
+        int miles = service.calculate(cost);
         System.out.println(miles);
     }
 }
+
+
+
+
+
+
+
+
+
+
+/*public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        boolean price = 12_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}*/
